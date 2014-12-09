3209dbf1b51847405daba712c3eeddfd2a15b873
simonthetemp <simonthetemp@gmail.com>
2014-12-09 21:53:25 +0100

    Affichage page rapport

--- fuel/app/views/temp/index.php
+++ fuel/app/views/temp/index.php
@@ -1,17 +1,19 @@
-<div>
-    <div>
-        Commandes livrées : <?php echo $count_delivered; ?>
-    </div>
-    <div>
-        Galettes : <?php echo $count_salt; ?>
-    </div>
-    <div>
-        Crêpes : <?php echo $count_sweet; ?>
-    </div>
-    <div>
-        Total : <?php echo $count_salt + $count_sweet; ?>
-    </div>
-    <div>
-        Ventes : <?php echo $sales; ?>
+<div class="panel panel-default">
+    <div class="panel-body" id="rapport">
+        <div>
+            Commandes livrées : <?php echo $count_delivered; ?>
+        </div>
+        <div>
+            Galettes : <?php echo $count_salt; ?>
+        </div>
+        <div>
+            Crêpes : <?php echo $count_sweet; ?>
+        </div>
+        <div>
+            Total : <?php echo $count_salt + $count_sweet; ?>
+        </div>
+        <div>
+            Ventes : <?php echo $sales; ?>
+        </div>
     </div>
 </div>
