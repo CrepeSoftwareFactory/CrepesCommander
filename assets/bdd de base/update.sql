af109fc98261fe19c97e0447b7e0d4e5691068d5
AurelGit <aurelien.mc@gmail.com>
2014-12-11 23:51:17 +0100

    base de données clean

--- assets/bdd de base/update.sql
+++ assets/bdd de base/update.sql
@@ -1,1 +1,137 @@
-ALTER TABLE  `product_order` ADD  `comment` VARCHAR( 255 ) NULL ;
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Base de données: `cc`
+--
+CREATE DATABASE IF NOT EXISTS `cc` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `cc`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `customer`
+--
+
+CREATE TABLE IF NOT EXISTS `customer` (
+  `customer_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `lastname` varchar(255) NOT NULL,
+  `firstname` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `zip` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ingredient`
+--
+
+CREATE TABLE IF NOT EXISTS `ingredient` (
+  `ingredient_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`ingredient_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `order`
+--
+
+CREATE TABLE IF NOT EXISTS `order` (
+  `order_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `date` datetime NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '0',
+  `customer_id` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `product`
+--
+
+CREATE TABLE IF NOT EXISTS `product` (
+  `product_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text,
+  `price` float NOT NULL,
+  `time` time DEFAULT NULL,
+  `type` int(11) NOT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `product_ingredient`
+--
+
+CREATE TABLE IF NOT EXISTS `product_ingredient` (
+  `product_id` bigint(20) unsigned NOT NULL,
+  `ingredient_id` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`product_id`,`ingredient_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `product_order`
+--
+
+CREATE TABLE IF NOT EXISTS `product_order` (
+  `product_order_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `product_id` bigint(20) unsigned NOT NULL,
+  `order_id` bigint(20) unsigned NOT NULL,
+  `start` datetime DEFAULT NULL,
+  `end` datetime DEFAULT NULL,
+  `priority` int(11) NOT NULL,
+  `price` float NOT NULL,
+  `station_id` bigint(20) unsigned DEFAULT NULL,
+  `free` tinyint(1) NOT NULL DEFAULT '0',
+  `comment` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`product_order_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `station`
+--
+
+CREATE TABLE IF NOT EXISTS `station` (
+  `station_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`station_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+
+
+INSERT INTO `user` (`user_id`, `login`, `password`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3');
+
+INSERT INTO `station` (`station_id`, `name`, `state`) VALUES
+(1, 'P1', 0),
+(2, 'P2', 0),
+(3, 'P3', 0),
+(4, 'P4', 0);
