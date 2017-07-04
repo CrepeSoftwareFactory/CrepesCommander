6fcb83e52048132cc1b459fac10a82ac9bc6de34
tJimbow <triomphe.jeanmarie@gmail.com>
2017-07-04 16:46:36 +0200

    - admin - product - order by
    - Ajout classement par type

--- fuel/app/classes/presenter/admin/index.php
+++ fuel/app/classes/presenter/admin/index.php
@@ -5,7 +5,11 @@ class Presenter_Admin_Index extends Presenter
     public function view()
     {
         $this->stations = Model_Station::find();
-        $this->products = Model_Product::find();
+        $this->products = Model_Product::find(function($query) {
+            $query
+                ->order_by('type', 'ASC')
+            ;
+        });
         $this->types = Model_Product_Type::find();
     }
 }
